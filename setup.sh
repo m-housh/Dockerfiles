e4320fd4c3270a75417e69c8bfc46176d9b324d7
Michael Housh <mhoush@houshhomeenergy.com>
2016-06-02 21:05:37 -0400

    Added bin and devpi-server.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
 
-CONFIG_PATH="$HOME/Dockerfiles/.config"
-SETUP_SCRIPTS_DIR="$HOME/Dockerfiles/.setup_scripts"
+HERE="$HOME/Dockerfiles"
+CONFIG_PATH="$HERE/.config"
+SETUP_SCRIPTS_DIR="$HERE/.setup_scripts"
+BIN="$HERE/bin"
+HOME_BIN="$HOME/.bin"
 
 
 
 main(){
     export CONFIG_PATH="$CONFIG_PATH"
+
+    if ! [ -d "$HOME_BIN" ]; then
+        mkdir "$HOME_BIN"
+    fi
+
+    for file in "$BIN"/*; do
+        if [ -f "$file" ]; then
+            ln -sv $file "$HOME_BIN/"
+        fi
+    done
     
     source "$SETUP_SCRIPTS_DIR/git_flow.sh"
 
