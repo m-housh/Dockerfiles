fe39c1f416dddaca5c6882405a0ad0824d6b611b
Michael Housh <mhoush@houshhomeenergy.com>
2016-06-07 19:51:26 -0400

    Updated bin/devpi-server.sh

--- bin/devpi-server.sh
+++ bin/devpi-server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-${DEVPI_DATA_DIR="$HOME/.devpi-data"}
+DEVPI_DATA_DIR="${HOME}/.devpi-data"
 ENV_FILE="$HOME/.env-files/devpi-server.env"
 
 if ! [ -f "$ENV_FILE" ]; then
@@ -10,14 +10,15 @@ fi
 
 server=$(docker ps --all --quiet --filter name="devpi-server")
 
-if [ "$server"  == "" ]; then
+
+if [[ "$server"  == "" ]]; then
     docker run -d --name devpi-server \
         -v "$DEVPI_DATA_DIR":/data \
         --env-file "$ENV_FILE" \
         -p "3141:3141" \
         mhoush/devpi-server --web
     exit 0
-elif [ "$server" != "" ]; then
+elif [[ "$server" != "" ]]; then
     docker start devpi-server
     exit 0
 else
