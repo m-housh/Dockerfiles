3306b58d2f8763988dec4b01af7bc88204fb8808
Michael Housh <mhoush@houshhomeenergy.com>
2016-07-27 18:08:28 -0400

    updated entrypoint.sh and README.md

--- py-dev/entrypoint.sh
+++ py-dev/entrypoint.sh
@@ -2,28 +2,26 @@
 
 set -e
 
-
-echo "PROJECT: ${PROJECT}"
-echo "PROJECT_PYTHON: ${PROJECT_PYTHON}"
-
-
-install(){
-
-    [ "${PROJECT}" != "" ] && \
-        pyenv virtualenv "${PROJECT_PYTHON}" "${PROJECT}" && \
-        pyenv local "${PROJECT}"
-
-    [ -f "${PWD}/requirements_dev.txt" ] && \
-        pyenv activate "${PROJECT}" && \
-        pip install --upgrade -r "${PWD}/requirements_dev.txt"
-}
-
 activate(){
 
     [ "${PROJECT}" != "" ] && \
         eval "$(pyenv init -)" && \
         eval "$(pyenv virtualenv-init -)" && \
         pyenv activate "${PROJECT}"
+}
+
+install(){
+
+    [ "${PROJECT}" != "" ] && \
+        pyenv virtualenv "${PROJECT_PYTHON}" "${PROJECT}" && \
+        pyenv local "${PROJECT}" && \
+        activate
+    
+    find /usr/src/app/ -name "requirements*.txt" \
+        -type f -exec pip install --upgrade -r {} \;
+
+    [ -f /usr/src/app/setup.py ] && \
+        pip install -e /usr/src/app/
 }
 
 main(){
